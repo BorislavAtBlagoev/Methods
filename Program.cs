ac093ac4e5f7604fd3d7320df22c3fe82e8575ca
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:32:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodExercises7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nxmMatrix = int.Parse(Console.ReadLine());
+            PrintMatrix(nxmMatrix);
+            Console.ReadLine();
+        }
+
+        static void PrintMatrix(int nxmMatrix)
+        {
+            for (int i = 0; i < nxmMatrix; i++)
+            {
+                for (int j = 0; j < nxmMatrix; j++)
+                {
+                    Console.Write(nxmMatrix + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
