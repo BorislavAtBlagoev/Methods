032ae1860bcf31f937da47bd38645d16d058aa64
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:31:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodExercises6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            MiddleLetterOfWord(word);
+            Console.ReadLine();
+        }
+        static void MiddleLetterOfWord(string word)
+        {
+            if (word.Length % 2 == 0)
+            {
+                Console.WriteLine((word[word.Length / 2 - 1]) + "" + (word[word.Length / 2]));
+            }
+            else
+            {
+                Console.WriteLine(word[word.Length / 2]);
+            }
+        }
+    }
+}
