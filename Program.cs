b05a137da883322b9d0e2b8b69640dc5bc90fe72
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:30:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodsExercises5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigits = int.Parse(Console.ReadLine());
+            int secondDigits = int.Parse(Console.ReadLine());
+            int lastDigits = int.Parse(Console.ReadLine());
+
+
+            int sumOfTwoDigits = SumOfTwoDigits(firstDigits, secondDigits);
+            Console.WriteLine(SubtractLastDigits(sumOfTwoDigits, lastDigits));
+
+            Console.ReadLine();
+        }
+
+        static int SumOfTwoDigits(int firstDigits, int secondDigits)
+        {
+            int sumOfDigits = firstDigits + secondDigits;
+
+            return sumOfDigits;
+        }
+
+        static int SubtractLastDigits(int sum, int lastDigits)
+        {
+            return sum - lastDigits;
+        }
+    }
+}
