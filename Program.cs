39b6551d75bbe4b0b742e811e43bd7bb65905cc6
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:26:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstDigits = int.Parse(Console.ReadLine());
+            int secondDigits = int.Parse(Console.ReadLine());
+            int thirdDigits = int.Parse(Console.ReadLine());
+
+            PrintSmallestDigit(firstDigits, secondDigits, thirdDigits);
+            Console.ReadLine();
+        }
+
+        static void PrintSmallestDigit(int firstDigits, int secondDigits, int thirdDigits)
+        {
+            if (firstDigits < secondDigits && firstDigits < thirdDigits)
+            {
+                Console.WriteLine(firstDigits);
+            }
+            else if (secondDigits < firstDigits && secondDigits < thirdDigits)
+            {
+                Console.WriteLine(secondDigits);
+            }
+            else 
+            {
+                Console.WriteLine(thirdDigits);
+            }
+                
+        }
+    }
+}
