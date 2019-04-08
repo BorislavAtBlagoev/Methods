778023f59d40b28f720e52463cce5cdd46872f08
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:37:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MethodMoreExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            CheckLenghtOfCoordinates(x1, y1, x2, y2, x3, y3, x4, y4);
+            Console.ReadLine();
+
+        }
+
+        static void CheckLenghtOfCoordinates(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            double distancePoint1 = Distance(x1, y1);
+            double distancePoint2 = Distance(x2, y2);
+            double distancePoint3 = Distance(x3, y3);
+            double distancePoint4 = Distance(x4, y4);
+
+
+            double lengthPoint12 = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            double lengthPoint34 = Math.Sqrt(Math.Pow((x3 - x4), 2) + Math.Pow((y3 - y4), 2));
+            if (lengthPoint12 >= lengthPoint34)
+            {
+                if (distancePoint1 <= distancePoint2)
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                else Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+            }
+            else if (lengthPoint12 < lengthPoint34)
+            {
+                if (distancePoint3 <= distancePoint4)
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                else Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+
+
+            }
+        }
+
+        public static double Distance(double x, double y)
+        {
+            double distance = Math.Sqrt((Math.Abs(x) * Math.Abs(x)) + (Math.Abs(y) * Math.Abs(y)));
+            return distance;
+        }
+    }
+}
