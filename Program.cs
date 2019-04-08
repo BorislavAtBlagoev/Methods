b8065573b32191467371b27ad654e4691ad4abbb
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:29:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodExercises3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            PrintCharBetweenTwo(firstChar, secondChar);
+            Console.ReadLine();
+
+        }
+
+        static void PrintCharBetweenTwo(char firstChar, char secondChar)
+        {
+            char temp = ' ';
+
+            if (firstChar>secondChar)
+            {
+                temp = firstChar;
+                firstChar = secondChar;
+                secondChar = temp;
+            }
+
+            for (int i = firstChar+1; i < secondChar; i++)
+            {
+                Console.Write((char)i+" ");
+            }
+
+        }
+
+    }
+}
