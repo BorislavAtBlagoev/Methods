ca578da899583cf4f08561aae36ba42c58264972
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:28:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodExercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(VowelsCount(input));
+        }
+
+        static int VowelsCount(string input)
+        {
+            int vowelsCount = 0;
+            string inputToLower = input.ToLower();
+
+
+            for (int i = 0; i < inputToLower.Length; i++)
+            {
+                if (inputToLower[i] == 'a' ||
+                    inputToLower[i] == 'o' ||
+                    inputToLower[i] == 'u' ||
+                    inputToLower[i] == 'e' ||
+                    inputToLower[i] == 'i' ||
+                    inputToLower[i] == 'y')
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+
+    }
+}
